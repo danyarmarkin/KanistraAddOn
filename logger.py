954c0c2c65dcf01634ff3075f459c5d252afef64
danyarmarkin <danila.iarmarkin@gmail.com>
2024-07-07 20:05:03 +0300

    bug fixed

--- logger.py
+++ logger.py
@@ -1,11 +1,6 @@
 def prepare():
     pass
-    # f = open("/Users/danyarmarkin/PycharmProjects/Kanistra-Add-On/tmp.txt", "w")
-    # f.close()
 
 
 def log(s):
     pass
-    # f = open("/Users/danyarmarkin/PycharmProjects/Kanistra-Add-On/tmp.txt", "a")
-    # f.write(f"{s}\n")
-    # f.close()
