cade291e0ba262a01ab4df8cb82a215a8a70c5a3
danyarmarkin <danila.iarmarkin@gmail.com>
2024-07-07 19:50:32 +0300

    adding update done

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Kanistra Client",
     "description": "",
     "author": "Kanistra Studio",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (4, 0, 0),
     "category": "Import-Export",
 }
